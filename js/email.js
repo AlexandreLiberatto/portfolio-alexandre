3b29597ccd46910c4245ddd57ba13f905bfd7138
unknown <alexandreliberatto@gmail.com>
2024-05-26 01:41:37 -0300

    Configurando api do gmail e implementando servico de e-mail, para que quando o usuario preencha mo formulario o conteudo seja enviado por e-mail.

---
+++ js/email.js
@@ -0,0 +1,31 @@
+/* ====Configura;'oes para o usu[ario envia os dados do form por e-mail==== */
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Inicialize o EmailJS com o seu user ID
+    emailjs.init("9tXYtReILnscqjyS-");
+
+    document.getElementById('form-contato').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const serviceID = 'service_u2zw90i';
+        const templateID = 'template_hi985re';  // Substitua pelo ID do seu template
+
+        const templateParams = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            numero: document.getElementById('numero').value,
+            assunto: document.getElementById('assunto').value,
+            mensagem: document.getElementById('mensagem').value
+        };
+
+        emailjs.send(serviceID, templateID, templateParams)
+            .then(function(response) {
+               console.log('ENVIADO!', response.status, response.text);
+               alert('Mensagem enviada com sucesso!');
+            }, function(error) {
+               console.log('FALHOU...', error);
+               alert('Falha ao enviar mensagem. Por favor, tente mais tarde.');
+            });
+    });
+});
+
