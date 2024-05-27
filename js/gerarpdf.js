00387d0005da2e122a75456ddd9e7ced8a3e30ba
unknown <alexandreliberatto@gmail.com>
2024-05-26 22:40:49 -0300

    Criando página de curriculo e adicionando função para baixar curriculo em pdf

---
+++ js/gerarpdf.js
@@ -0,0 +1,54 @@
+/* ========Gerador de PDF======= */
+
+
+// Esta função será responsável por gerar o PDF a partir do conteúdo do curriculo.html
+function generatePDF() {
+    // Carrega o conteúdo do arquivo curriculo.html
+    fetch('curriculo.html')
+        .then(response => response.text())
+        .then(html => {
+            console.log("Conteúdo do curriculo.html:", html); // Adiciona um log para verificar o conteúdo
+
+            // Agora temos o conteúdo HTML do currículo
+            // Vamos gerar o PDF a partir desse HTML
+            var element = document.createElement('div');
+            element.innerHTML = html;
+
+            // Configurações para a biblioteca html2pdf
+            var opt = {
+                margin:       1,
+                filename:     'meu_curriculo.pdf',
+                image:        { type: 'jpeg', quality: 0.98 },
+                html2canvas:  { scale: 2 },
+                jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+
+            // Gera o PDF
+            html2pdf()
+                .from(element)
+                .set(opt)
+                .save();
+        })
+        .catch(error => {
+            console.error("Erro ao carregar o curriculo.html:", error); // Adiciona um log de erro
+        });
+}
+
+// Captura o evento de clique no botão de download do currículo
+document.getElementById('download').addEventListener('click', function() {
+    // Chama a função para gerar o PDF
+    generatePDF();
+}); 
+
+
+/* =====menu===== */
+document.addEventListener('DOMContentLoaded', () => {
+    const menuHamburguer = document.querySelector('.menu-hamburguer');
+    const nav = document.querySelector('.nav');
+  
+    menuHamburguer.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  });
+  
+
